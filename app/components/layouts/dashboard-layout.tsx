1ecd0e1b9af65d140fb3ea86a09abf3368b435de
achmedhibatillah <ach.hibatillah@gmail.com>
2026-02-28 23:16:37 +0700

    fix layout

--- app/components/layouts/dashboard-layout.tsx
+++ app/components/layouts/dashboard-layout.tsx
@@ -1,12 +1,45 @@
 import { SidebarProvider, SidebarTrigger } from "~/components/ui/sidebar"
 import DashboardSidebar from "./dashboard-sidebar"
 import type React from "react"
+import { Card, CardContent } from "../ui/card"
+import { Tooltip, TooltipContent, TooltipTrigger } from "../ui/tooltip"
+import { GraduationCap, Home, UserRound } from "lucide-react"
+import type { ReactNode } from "react"
+import type { menuItemsType } from "../types/layout-type"
 
 const DashboardLayout = ({
     children
 }: {
     children: React.ReactNode
 }) => {
+
+    const menus: menuItemsType[] = [
+        {
+            name: "Dashboard",
+            url: "/",
+            icon: <Home />,
+            sub: null
+        },
+        {
+            name: "Siswa",
+            url: "",
+            icon: <GraduationCap />,
+            sub: [
+                {
+                    name: "Database Siswa",
+                    url: "/students",
+                    icon: null,
+                    sub: null
+                },
+                {
+                    name: "Tambah Data Siswa",
+                    url: "/students/add",
+                    icon: null,
+                    sub: null
+                },
+            ]
+        }
+    ]
     return (
         <SidebarProvider
             style={
@@ -16,9 +49,18 @@ const DashboardLayout = ({
                 } as React.CSSProperties
             }
         >
-            <DashboardSidebar />
-            <main>
-                <SidebarTrigger />
+            <DashboardSidebar menus={menus} />
+            <main className="bg-gray-200 p-2 flex flex-col w-full">
+                <Card className="p-2 mb-2 w-full sticky">
+                    <CardContent className="px-2">
+                        <div className="flex items-center">
+                            <SidebarTrigger />
+                            <div className="ml-auto flex items-center">
+                                <div className="bg-blue-300 rounded-full w-8 h-8 flex items-center justify-center cursor-pointer transition-all duration-200 hover:bg-blue-400"><UserRound className="h-5 text-white" /></div>
+                            </div>
+                        </div>
+                    </CardContent>
+                </Card>
                 {children}
             </main>
         </SidebarProvider>
---
+++ app/routes/students-add.tsx
@@ -0,0 +1,11 @@
+import DashboardLayout from "~/components/layouts/dashboard-layout"
+
+const StudentsAddPage = () => {
+    return (
+        <DashboardLayout>
+            <>Content</>
+        </DashboardLayout>
+    )
+}
+
+export default StudentsAddPage
